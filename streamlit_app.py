0305c68b88e9b2656a8fa977a9392d9b8cb1a050
Ekkachail <154778937+Ekkachail@users.noreply.github.com>
2023-12-25 14:58:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,9 @@ import streamlit as st
 """
 # เอกชัย หลายศิริกุล
 
+st.markdown('_Markdown_') # see #*
+
+
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
