a4cfc391d17002ad1d4eb504226061ca3aa220d0
Ekkachail <154778937+Ekkachail@users.noreply.github.com>
2023-12-26 12:12:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,15 +57,7 @@ st.code('for i in range(8): foo()')
 # * optional kwarg unsafe_allow_html = True
 
 
-# Show a spinner during a process
->>> with st.spinner(text='In progress'):
->>>   time.sleep(3)
->>>   st.success('Done')
 
-# Show and update progress bar
->>> bar = st.progress(50)
->>> time.sleep(3)
->>> bar.progress(100)
 
 st.balloons()
 st.snow()
