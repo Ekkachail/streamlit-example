e764c595d2a914f1a4ff0d0482782a18177cd96b
Ekkachail <154778937+Ekkachail@users.noreply.github.com>
2023-12-27 19:37:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -119,8 +119,8 @@ st.title("Streamlit Matplotlib with Thai Language")
 
 # Matplotlib plot
 fig, ax = plt.subplots()
-ax.plot([1, 2, 3], label="ก", marker="o")
-ax.plot([3, 2, 1], label="ข", marker="x")
+ax.plot([1, 2, 3], label="เอกชัย", marker="o")
+ax.plot([3, 2, 1], label="หลายศิริกุล", marker="x")
 ax.set_xlabel("เวลา (Time)")
 ax.set_ylabel("ค่า (Value)")
 ax.legend()
